fcdc8059d297ca193d39af0e832f7564607b5f92
Arch <topanic@qq.com>
2024-04-08 15:34:28 +0800

    pa2 phase2: add "malloc" simple implement

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,12 +29,28 @@ int atoi(const char* nptr) {
   return x;
 }
 
+static uintptr_t addr = 0;  // 上次分配内存的位置
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+//  panic("Not implemented");
+
+  if (addr == 0) {  // 如果是第一次调用malloc
+    addr = (uintptr_t)heap.start;  // 从堆区开始分配
+  }
+
+  if (addr + size > (uintptr_t)heap.end) {  // 如果堆区剩余空间不足
+    return NULL;  // 返回NULL表示分配失败
+  }
+
+  void *ret = (void *)addr;  // 返回的内存区域的起始地址
+  addr += size;  // 更新addr
+
+  return ret;
+
 #endif
   return NULL;
 }
