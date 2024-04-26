86f107a2096148fb742b8a0d460c548ba0e16f90
Arch <topanic@qq.com>
2024-04-26 18:10:37 +0800

    pa3 phase2: finish a part of syscall

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,37 @@
 #include <common.h>
 #include "syscall.h"
-void do_syscall(Context *c) {
-  uintptr_t a[4];
-  a[0] = c->GPR1;
 
-  switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
-  }
+enum Syscall_type {
+    SYS_exit,
+    SYS_yield,
+};
+
+void sys_exit(Context *c);
+void sys_yield(Context *c);
+
+void do_syscall(Context *c) {
+    uintptr_t a[4];
+    a[0] = c->GPR1;
+
+    switch (a[0]) {
+        case SYS_exit:
+            sys_exit(c);
+            break;
+        case SYS_yield:
+            sys_yield(c);
+            break;
+
+        default:
+            panic("Unhandled syscall ID = %d", a[0]);
+    }
+}
+
+void sys_exit(Context *c) {
+    halt(c->GPRx);
+}
+
+void sys_yield(Context *c) {
+    yield();
+    // set return value
+    c->GPRx = 0;
 }
