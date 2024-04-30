ed27180bcfa409a730278f6a78f769bab02d7bfe
Arch <topanic@qq.com>
2024-04-30 10:46:57 +0800

    pa3 phase3: complete NSlider

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,12 +1,18 @@
 #include <NDL.h>
 #include <SDL.h>
 
+#include <string.h>
+#include <assert.h>
+
 #define keyname(k) #k,
 
 static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
 };
+
+#define NR_KEYS ( sizeof(keyname)/sizeof(keyname[0]) )
+static uint8_t keysnap[NR_KEYS] = {0};
 
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
@@ -17,7 +23,30 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+#define BUF_LEN 16
+    char buf[BUF_LEN];
+    memset(buf, 0, BUF_LEN);
+
+    while (NDL_PollEvent(buf, BUF_LEN) == 0);
+    // printf("event is %s in SDL\n", buf);
+    if (strncmp(buf, "kd", 2) == 0) {
+        // printf("down\n");
+        event->type = SDL_KEYDOWN;
+    } else if (strncmp(buf, "ku", 2) == 0) {
+        // printf("up\n");
+        event->type = SDL_KEYUP;
+    }
+
+    for (size_t i = 0; i < NR_KEYS; ++i) {
+        if (((strlen(buf + 3)) == strlen(keyname[i])) && (strncmp(buf + 3, keyname[i], strlen(keyname[i])) == 0)) {
+            // printf("keyname hit %s\n", keyname[i]);
+            event->key.keysym.sym = i;
+            keysnap[i] = (event->type == SDL_KEYDOWN) ? 1 : 0;
+            break;
+        }
+    }
+
+    return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
